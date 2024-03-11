54c463d77e84a64532788d576dc68bab2ba43f81
Sujith-Kumar-2003 <sujithkumar2003k@gmail.com>
2024-03-11 00:02:59 -0400

    The site is up.

---
+++ src/components/app/app.module.ts
@@ -0,0 +1,14 @@
+@NgModule({
+  declarations: [
+    // other components
+    DashboardComponent
+  ],
+  imports: [
+    RouterModule.forRoot(routes)
+    // other modules
+  ],
+  providers: [],
+  bootstrap: [/* your bootstrap component */]
+})
+export class AppModule { }
+
--- src/components/dashboard/dashboard.component.ts
+++ src/components/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  title = "DashBoard"
 }
+
